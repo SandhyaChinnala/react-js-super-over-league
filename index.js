d4e5b1d83b5e82ae3d51085c21edb128276db780
SandhyaChinnala <o162046@rguktong.ac.in>
2022-12-14 08:26:43 +0000

    React- super-over-league

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
